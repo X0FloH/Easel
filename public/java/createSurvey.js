d9d164d54635ed2db8498427eb566749aaa5b52d
X0FloH <oscar@rwjones.info>
2019-12-05 17:20:17 +0000

    Trying other thing with require.js

---
+++ public/java/createSurvey.js
@@ -0,0 +1,21 @@
+var currentAmountQuestions = 0;
+
+function updateList(){
+  if(currentAmountQuestions < 5){
+    currentAmountQuestions += 1;
+    document.getElementById("listBox").style="height:" + ((currentAmountQuestions * 9) + 10) + "vh";
+    document.getElementById("addQuestion").style="left:40%;width:15%;height:7vh;padding-top:0%;top:" + (((currentAmountQuestions+1)*9)-7) + "vh;";
+    document.getElementById("questionBox" + currentAmountQuestions).removeAttribute("hidden");
+  }
+}
+
+function createSurvey(){
+  var randNumStr = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
+  var fs = require('fs');
+  fs.writeFile("Surveys/" + randNumStr + '.js', " survey \n" + document.getElementById('question1Input').value + " \n" + document.getElementById('question2Input').value + " \n" + document.getElementById('question3Input').value + " \n" + document.getElementById('question4Input').value + " \n" + document.getElementById('question5Input').value, function(err) {
+    if(err) throw err;
+    console.log("Created " + randNumStr);
+  });
+}
+
+setTimeout(() => {updateList();}, 100);
