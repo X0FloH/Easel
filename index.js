d466e1d1dba669f13107ed9521fb8564b974b664
X0FloH <oscar@rwjones.info>
2019-12-05 19:41:48 +0000

    Added home page and other functionality

--- index.js
+++ index.js
@@ -9,16 +9,33 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/create', (req, res) => res.render('pages/create'))
   .get('/createSurvey', (req, res) => {
-    var randNum = req.query['id'] || ' ';
-    var ques1 = req.query['q1'] || ' ';
-    var ques2 = req.query['q2'] || ' ';
-    var ques3 = req.query['q3'] || ' ';
-    var ques4 = req.query['q4'] || ' ';
-    var ques5 = req.query['q5'] || ' ';
+    var randNum = req.query['id'] || '';
+    var ques1 = req.query['q1'] || '';
+    var ques2 = req.query['q2'] || '';
+    var ques3 = req.query['q3'] || '';
+    var ques4 = req.query['q4'] || '';
+    var ques5 = req.query['q5'] || '';
+    var fileString = "";
+    if(ques1 != ''){
+      fileString += ques1 + " /n";
+    }
+    if(ques2 != ''){
+      fileString += ques2 + " /n";
+    }
+    if(ques3 != ''){
+      fileString += ques3 + " /n";
+    }
+    if(ques4 != ''){
+      fileString += ques4 + " /n";
+    }
+    if(ques5 != ''){
+      fileString += ques5;
+    }
     var fs = require('fs');
-    fs.writeFile("public/Surveys/" + randNum + '.js', " survey \n" + ques1 + " \n" + ques2 + " \n" + ques3 + " \n" + ques4 + " \n" + ques5, function(err) {
+    fs.writeFile("public/Surveys/" + randNum + '.js', " survey \n" + fileString, function(err) {
       if(err) throw err;
       res.render('pages/creating');
     });
   })
+  .get('/home', (req, res) => req.render('pages/home'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
--- public/Surveys/First.js
+++
@@ -1,5 +0,0 @@
-4
-What is your name?
-What is your email?
-Why did you stop using our service?
-How could we improve our service?
