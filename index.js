cefaa1d961c0b061489cd7b63589de6e2dd2c9d5
X0FloH <oscar@rwjones.info>
2019-12-05 18:48:38 +0000

    Fixed using old APIs

--- index.js
+++ index.js
@@ -10,18 +10,16 @@ express()
   .get('/create', (req, res) => res.render('pages/create'))
   .get('/createSurvey', (req, res) => {
     var randNum = req.param('id');
-    var ques1 = req.param('q1');
-    var ques2 = req.param('q2');
-    var ques3 = req.param('q3');
-    var ques4 = req.param('q4');
-    var ques5 = req.param('q5');
-    console.log("Trying to create");
-    res.render('pages/creating');
+    var ques1 = req.query['q1'] || ' ';
+    var ques2 = req.query['q2'] || ' ';
+    var ques3 = req.query['q3'] || ' ';
+    var ques4 = req.query['q4'] || ' ';
+    var ques5 = req.query['q5'] || ' ';
     var fs = require('fs');
     fs.writeFile("public/Surveys/" + randNumStr + '.js', " survey \n" + ques1 + " \n" + ques2 + " \n" + ques3 + " \n" + ques4 + " \n" + ques5, function(err) {
       if(err) throw err;
-      console.log("Created " + randNumStr);
       window.location.href = "/?survey=" + randNum;
     });
+    res.render('pages/creating');
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
