d613899522db5f6e84c390bd8288d523e2d4c347
X0FloH <oscar@rwjones.info>
2019-12-10 21:23:13 +0000

    Trying to fix answers page 12

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ express()
   })
   .get('/answers', (req, res) => {
     var fs = require('fs');
-    ArrayList<String> allAnswers = new ArrayList<String>();
+    var allAnswers = new ArrayList<String>();
     fs.readdirSync('public/Answers/' + req.query['id']).forEach(file => {
       console.log(file);
       allAnswers.add(file);
